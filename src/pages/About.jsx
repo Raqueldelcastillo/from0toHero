a7bcd2ee35c4c91804df8cfde8aeb8c585024590
Raquel del Castillo <117642803+Raqueldelcastillo@users.noreply.github.com>
2024-01-23 20:26:01 +0000

    add router and text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,11 @@
 export default function About() {
-  return <div>About...........</div>;
+  return (
+    <div>
+      We are three girls that have decided to gather and compile all the
+      resources and information from our FrontEnd bootcamp and make it into our
+      last project
+      <br />
+      We hope it helps you as much as it has helped creating for you.
+    </div>
+  );
 }
