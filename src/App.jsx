e67234d625f27e1a8ea73259e30c23a6b22eaab2
Raquel del Castillo <117642803+Raqueldelcastillo@users.noreply.github.com>
2024-01-24 21:32:43 +0000

    new Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
-import React from "react";
-import "./App.css";
-import TopNav from "./components/navbar/TopNav";
-import Caroussel from "./components/caroussel/caroussel";
-import SearchBox from "./components/searchBox/seachBar";
-import HeroSection from "./components/hero/Hero";
-import Cardcontainer from "./components/ResourcesCards/cardcontainer";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NewsletterForm from './components/newletterForm/NewsletterForm';
+// import React from "react";
+// import "./App.css";
+// import TopNav from "./components/navbar/TopNav";
+// import About from "./pages/About";
+// import Resources from "./pages/Resources";
+// import Caroussel from "./components/caroussel/caroussel";
+// import SearchBox from "./components/searchBox/seachBar";
+// import HeroSection from "./components/hero/Hero";
+// import Cardcontainer from "./components/ResourcesCards/cardcontainer";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import NewsletterForm from "./components/newletterForm/NewsletterForm";
 
-function App() {
-  return (
-    <div>
-      <div>
-        <TopNav />
-        <HeroSection />
-        <SearchBox />
-        <Caroussel />
-        <Cardcontainer />
-        < NewsletterForm />
-      </div>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <>
+//       <div>
+//         <Routes>
+//         <TopNav />
+//         <HeroSection />
+//         <SearchBox />
+//         <Caroussel />
+//         <Cardcontainer />
+//         <NewsletterForm />
+        
+        
+//           {/* Define routes using the Route component to render different page components at different paths */}
+//           {/* Define a default route that will render the Home component */}
+//           {/* <Route path="/" element={<Home />} /> */}
+//           <Route path="/about" element={<About />} />
+//           {/* <Route path="blog" element={<Blog />} /> */}
+//           {/* Define a route that will have descendant routes */}
+//           {/* <Route path="contact/*" element={<Contact />} /> */}
+//         </Routes>
+//         <Resources />
+//       </Router>
+//       </div>
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/components/navbar/TopNav.jsx
+++ src/components/navbar/TopNav.jsx
@@ -1,57 +1,3 @@
-// import React from "react";
-// import {
-//   Collapse,
-//   Navbar,
-//   NavbarToggler,
-//   NavbarBrand,
-//   Nav,
-//   NavItem,
-//   NavLink,
-// } from "reactstrap";
-
-// export default class TopNav extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.toggle = this.toggle.bind(this);
-//     this.state = {
-//       isOpen: false,
-//     };
-//   }
-//   toggle() {
-//     this.setState({
-//       isOpen: !this.state.isOpen,
-//     });
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <Navbar color="faded" light toggleable>
-//           <NavbarToggler right onClick={this.toggle} />
-//           <NavbarBrand href="/">Navigate</NavbarBrand>
-//           <Collapse isOpen={this.state.isOpen} navbar>
-//             <Nav className="ml-auto" navbar>
-//               <NavItem>
-//                 <NavLink href="/components/">About Us</NavLink>
-//               </NavItem>
-//               <NavItem>
-//                 <NavLink href="https://github.com/reactstrap/reactstrap">
-//                   Road Map
-//                 </NavLink>
-//               </NavItem>
-//               <NavItem>
-//                 <NavLink href="https://github.com/reactstrap/reactstrap">
-//                   Resourses
-//                 </NavLink>
-//               </NavItem>
-//             </Nav>
-//           </Collapse>
-//         </Navbar>
-//       </div>
-//     );
-//   }
-// }
-
 import React, { useState } from "react";
 import {
   Collapse,
@@ -62,23 +8,20 @@ import {
   NavItem,
   NavLink,
 } from "reactstrap";
-
-function Example(props) {
+function Navigate(props) {
   const [collapsed, setCollapsed] = useState(true);
-
   const toggleNavbar = () => setCollapsed(!collapsed);
-
   return (
     <div>
       <Navbar color="faded" light>
         <NavbarBrand href="/" className="me-auto">
-          Navigate
+          From Zero to Hero
         </NavbarBrand>
         <NavbarToggler onClick={toggleNavbar} className="me-2" />
         <Collapse isOpen={!collapsed} navbar>
           <Nav navbar>
             <NavItem>
-              <NavLink href="/components/">About Us</NavLink>
+              <NavLink href="/about">About Us</NavLink>
             </NavItem>
             <NavItem>
               <NavLink href="https://raqueldelcastillo.github.io/beanOpedia/">
@@ -86,7 +29,7 @@ function Example(props) {
               </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/components/">Resources</NavLink>
+              <NavLink href="#resources">Resources</NavLink>
             </NavItem>
           </Nav>
         </Collapse>
@@ -94,5 +37,4 @@ function Example(props) {
     </div>
   );
 }
-
-export default Example;
+export default Navigate;
